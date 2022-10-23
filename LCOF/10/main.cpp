9c9ea37a315ddd5f4aab8d7c6165fde187abf700
Vito-Li-97 <84769457+Vito-Li-97@users.noreply.github.com>
2022-10-23 21:00:18 +0800

    221023

--- LCOF/10/main.cpp
+++ LCOF/10/main.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution {  // 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）
  public:
+  // 方法一：动态规划
+  // O(n) O(1)
   int fib(int n) {
     int f0 = 0;
     int f1 = 1;
@@ -19,15 +21,53 @@ class Solution {
 
     return f2;
   }
+
+  //方法二：矩阵快速幂
+  // O(logn) O(1)
+  const long MOD = 1000000007;
+
+  vector<vector<long long>> multiply_matrix(vector<vector<long long>> a, vector<vector<long long>> b) {
+    vector<vector<long long>> result{{0, 0}, {0, 0}};
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        result[i][j] = (a[i][0] * b[0][j] + a[i][1] * b[1][j]) % MOD;
+      }
+    }
+    return result;
+  }
+
+  vector<vector<long long>> pow_matrix(vector<vector<long long>> M, int n) {
+    vector<vector<long long>> result = {{1, 0}, {0, 1}};
+    while (n > 0) {
+      if (n & 1) {  // 若n的二进制最末为 1
+        result = multiply_matrix(result, M);
+      }
+      M = multiply_matrix(M, M);
+      n = n >> 1;
+    }
+    return result;
+  }
+
+  int fib_2(int n) {
+    if (n < 2) {
+      return n;
+    }
+    vector<vector<long long>> M = {{1, 1}, {1, 0}};
+    vector<vector<long long>> M_n = pow_matrix(M, n - 1);
+    int result = (int)M_n[0][0];
+    return result;
+  }
 };
 
 int main() {
-  int n = 100;
+  int n = 46;
 
   Solution n1;
-  int ans = n1.fib(100);
+  int ans = n1.fib(n);
+  int ans2 = n1.fib_2(n);
 
-  cout << ans << ' ';
+  cout << ans << endl
+       << ans2 << endl;
 
   return 1;
 }
